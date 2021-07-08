9a470dec3eb0358ac29170265a86c8fcbecb27b1
Jorge Calheiros <jorgecalheiros.s@gmail.com>
2021-07-07 21:30:54 -0300

    Arrumei uma letra de estava errada

--- php/conexao.php
+++ php/conexao.php
@@ -5,7 +5,7 @@
   $BD = 'dbusuarios';
   try {
     $pdo=new PDO("mysql:dbname=$BD;host=$host","$usuario","$senha");
-  } catch (PDPException $e) {
+  } catch (PDOException $e) {
     echo "Erro com o banco de dados: ".$e->getMessage();
   } catch(Exception $e){
     echo "Erro generico: ".$e->getMessage();
